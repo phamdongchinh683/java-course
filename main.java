6cb3678cfd3911f04209d39092afeac675ec266e
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-08-30 08:27:54 +0700

    boolean type 30-8-2025

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 class main {
  public static void main(String[] args) {
-  System.out.println("Hello world");
+  boolean b1 = true;
+  boolean b2 = false;
+  // Size : Virtual machine dependent (typically 1 byte, 8 bits)
+  System.out.println("Is Java fun? " + b1); // Is Java fun? true
+  System.out.println("Is fish tasty? " + b2); // Is fish tasty? false
  }
 }
