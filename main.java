c725816ae93473d5b8b46a9a7f28e29f6d546279
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-08-30 08:45:43 +0700

    char type 30-8-2025

--- main.java
+++ main.java
@@ -1,10 +1,9 @@
 class main {
  public static void main(String[] args) {
-  double pi = 3.141592653589793;
-  double an = 6.02214076e23;
-  // Size : 8 bytes (64 bits)
-  // ~15-16 significant decimal digits
-  System.out.println("Value of Pi: " + pi); // Value of Pi: 3.141592653589793
-  System.out.println("Avogadro's Number: " + an); // Avogadro's Number: 6.02214076E23
+  char g = 'A';
+  char s = '$';
+  // Size : 2 bytes (16 bits)
+  System.out.println("Grade: " + g); // Grade: A
+  System.out.println("Symbol: " + s); // Symbol: $
  }
 }
