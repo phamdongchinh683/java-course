bf3ed2d6f489af1f6e4b46df0fbb3db52e467129
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-08-30 08:40:45 +0700

    long type 30-8-2025

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
 class main {
  public static void main(String[] args) {
-  int p = 2000000;
-  int d = 150000000;
-  // Size : 4 bytes ( 32 bits )
-  // -2,147,483,648 to 2,147,483,647
-  System.out.println("Population: " + p); // Population: 2000000
-  System.out.println("Distance: " + d); // Distance: 150000000
+  long w = 7800000000L;
+  long l = 9460730472580800L;
+  // Size : 8 bytes (64 bits)
+  // -9,223,372,036,854,775,80 to 9,223,372,036,854,775,807
+  System.out.println("World Population: " + w); // World Population: 7800000000
+  System.out.println("Light Year Distance: " + l); // Light Year Distance: 9460730472580800
 
  }
 }
