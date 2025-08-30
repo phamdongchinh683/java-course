f48f0c0c5ffb1d3adc82b76ab24f7af218e212b7
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-08-30 08:33:05 +0700

    byte type 30-8-2025

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 class main {
  public static void main(String[] args) {
-  boolean b1 = true;
-  boolean b2 = false;
-  // Size : Virtual machine dependent (typically 1 byte, 8 bits)
-  System.out.println("Is Java fun? " + b1); // Is Java fun? true
-  System.out.println("Is fish tasty? " + b2); // Is fish tasty? false
+  byte a = 29;
+  byte t = -10;
+  // 1 byte
+  // Size: -128 to 127
+
+  // error: incompatible types: possible lossy conversion from int to byte
+  byte c = 129;
+  byte d = -129;
+
+  System.out.println("Age: " + a);
+  System.out.println("Temperature: " + t);
  }
 }
