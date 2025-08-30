e57342083402f04cc17f5e5d6da2f1b3811386ff
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-08-30 08:38:54 +0700

    int type 30-8-2025

--- main.java
+++ main.java
@@ -1,10 +1,11 @@
 class main {
  public static void main(String[] args) {
-  short num = 1000;
-  short t = -200;
-  // Size : 2 bytes (16 bits)
-  // -32,768 to 32,767
-  System.out.println("Number of Students: " + num); // Number of Students: 1000
-  System.out.println("Temperature: " + t); // Temperature: -200
+  int p = 2000000;
+  int d = 150000000;
+  // Size : 4 bytes ( 32 bits )
+  // -2,147,483,648 to 2,147,483,647
+  System.out.println("Population: " + p); // Population: 2000000
+  System.out.println("Distance: " + d); // Distance: 150000000
+
  }
 }
