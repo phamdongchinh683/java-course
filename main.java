7a07f538331e5b7d9eb1c58c6a250b9c1acba714
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-09-04 07:52:13 +0700

    local variable 4-9-2025

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 class main {
  public static void main(String[] args) {
-  int[] num = { 1, 2, 3, 4, 5 };
-  String[] arr = { "Geek1", "Geek2", "Geek3" };
+// Declared a Local Variable
+int var = 10;
 
-  System.out.println("First Number: " + num[0]); // First Number: 1
-  System.out.println("Second Fruit: " + arr[1]); // Second Fruit: Geek2
+// This variable is local to this main method only
+System.out.println("Local Variable: " + var);
  }
 }
