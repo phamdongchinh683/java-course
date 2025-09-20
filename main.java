3a3105bd0c8b2c65c1d6df227426d27cd2d58322
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-09-20 07:41:27 +0700

    example Encapsulation 20-9-2025

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
-import java.util.Arrays;
-import java.util.List;
 
 class main {
- 
+ public static void main(String[] args) {
+  Animal animal = new Animal("Buddy");
+  animal.makeSound();
  }
+
+}
